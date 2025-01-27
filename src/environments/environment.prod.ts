3c697504e0c267e7e7e6d8869aaf9c840ef90ee6
Roman Mikaelyan <romanmik@yandex.ru>
2025-01-27 12:37:44 +0300

    feat: change api url path

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://thebloodofazra.com',
+  api_url: 'https://thebloodofazra.com/s',
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  api_url: 'https://thebloodofazra.com',
+  api_url: 'https://thebloodofazra.com/s',
 }
